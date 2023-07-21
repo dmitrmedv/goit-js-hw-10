62486a0490e30795b9987f8b61bbf30f0b705b19
dmitrmedv <dmitrmedv@gmail.com>
2023-07-22 00:38:48 +0300

    add

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
-export default function fetchBreeds() {
-  return axios
-    .get('https://api.thecatapi.com/v1/breeds')
-    .then(resp => resp.data);
+const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
+axios.defaults.headers.common['x-api-key'] =
+  'live_lYmOKTQRHsB8sJe9ygzuQe7FXcc2ZvtWiVnNozCi85ub5YUZiLk8YnrbwAneZI04';
+
+export function fetchBreeds() {
+  return axios.get('https://api.thecatapi.com/v1/breeds');
+}
+
+export function fetchCatByBreed(breedId) {
+  return axios.get(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,50 @@
-import { fetchCat } from './fetchCat';
-import fetchBreeds from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const breedSelectRef = document.querySelector('.breed-select');
+const catInfoRef = document.querySelector('.cat-info');
+const loaderRef = document.querySelector('.loader');
+const errorRef = document.querySelector('.error');
 
-fetchBreeds().then(renderOptions(markup));
+loaderRef.style.display = 'none';
+errorRef.style.display = 'none';
 
-function markup(data) {
+breedSelectRef.addEventListener('change', selectCat);
+
+function selectCat(event) {
+  catInfoRef.innerHTML = '';
+  loaderRef.style.display = 'block';
+  setTimeout(() => {
+    fetchCatByBreed(event.target.value)
+      .then(({ data }) => {
+        let { name, description, temperament } = data[0].breeds[0];
+        let { url } = data[0];
+        catInfoRef.innerHTML = markupCatCard({
+          name,
+          description,
+          temperament,
+          url,
+        });
+      })
+      .catch(error => (errorRef.style.display = 'block'))
+      .finally((loaderRef.style.display = 'none'));
+  }, 3000);
+}
+
+fetchBreeds()
+  .then(({ data }) => {
+    breedSelectRef.innerHTML = markupOptions(data);
+  })
+  .catch(error => console.log(error));
+
+function markupOptions(data) {
   return data
-    .map(({ id, name }) => {
-      return `<option value=${id}>${name}</option>`;
-    })
+    .map(({ id, name }) => `<option value=${id}>${name}</option>`)
     .join('');
 }
 
-function renderOptions(el) {
-  breedSelectRef.insertAdjacentElement('beforebegin', el);
+function markupCatCard({ name, description, temperament, url }) {
+  return `<h2>${name}</h2>
+    <img src=${url} alt="${name}" width="350px" />
+    <p>${description}</p>
+    <p>temperament: ${temperament}</p>`;
 }
