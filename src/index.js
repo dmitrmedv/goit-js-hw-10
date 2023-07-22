d5b2a839dbde9cb55e4cc136ae987105c97cc07c
dmitrmedv <dmitrmedv@gmail.com>
2023-07-22 19:54:35 +0300

    add

--- src/index.js
+++ src/index.js
@@ -13,21 +13,21 @@ breedSelectRef.addEventListener('change', selectCat);
 function selectCat(event) {
   catInfoRef.innerHTML = '';
   loaderRef.style.display = 'block';
-  setTimeout(() => {
-    fetchCatByBreed(event.target.value)
-      .then(({ data }) => {
-        let { name, description, temperament } = data[0].breeds[0];
-        let { url } = data[0];
-        catInfoRef.innerHTML = markupCatCard({
-          name,
-          description,
-          temperament,
-          url,
-        });
-      })
-      .catch(error => (errorRef.style.display = 'block'))
-      .finally((loaderRef.style.display = 'none'));
-  }, 3000);
+  // setTimeout(() => {
+  fetchCatByBreed(event.target.value)
+    .then(({ data }) => {
+      let { name, description, temperament } = data[0].breeds[0];
+      let { url } = data[0];
+      catInfoRef.innerHTML = markupCatCard({
+        name,
+        description,
+        temperament,
+        url,
+      });
+    })
+    .catch(error => (errorRef.style.display = 'block'))
+    .finally((loaderRef.style.display = 'none'));
+  // }, 3000);
 }
 
 fetchBreeds()
@@ -43,8 +43,10 @@ function markupOptions(data) {
 }
 
 function markupCatCard({ name, description, temperament, url }) {
-  return `<h2>${name}</h2>
-    <img src=${url} alt="${name}" width="350px" />
+  return `
+    <img src=${url} alt="${name}"/>
+    <div class ='infobox'>
+    <h2>${name}</h2>
     <p>${description}</p>
-    <p>temperament: ${temperament}</p>`;
+    <p><span>Temperament:</span> ${temperament}</p>`;
 }
